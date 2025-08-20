40175a01a03eb1f2da35dde474d4aa5df590fc9e
Shivam <shivamrai6836@gmail.com>
2025-08-20 14:04:03 +0530

    server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-import expres from "express";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/api/v2/health", (req, res) => {
+  return res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Protected server is running on port ${PORT}`);
+});
